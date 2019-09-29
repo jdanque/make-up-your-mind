fc8eff5ee5cd17f2c393e7ec5bcc3620d7fdd83b
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2019-09-29 13:01:50 +0800

    feat: disable react dev tools on production

--- src/main/pages/Root.js
+++ src/main/pages/Root.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function Root() {
-  return (
-    <div className="Root">
-      
-    </div>
-  );
+class Root extends Component {
+  componentDidMount() {
+    this.disableReactDevTools();
+  }
+
+  // Only in production
+  disableReactDevTools = () => {
+    if (process.env.NODE_ENV === "production") {
+      if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+        for (const [key, value] of Object.entries(
+          window.__REACT_DEVTOOLS_GLOBAL_HOOK__
+        )) {
+          window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] =
+            typeof value === "function" ? () => {} : null;
+        }
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div id="root" className="root">
+      </div>
+    );
+  }
 }
 
 export default Root;
