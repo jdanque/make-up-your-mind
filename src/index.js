32cdab5e19481ca883848aea74232566af65e462
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2019-09-29 12:45:09 +0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './main/components/Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
---
+++ src/main/pages/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Root() {
+  return (
+    <div className="Root">
+      
+    </div>
+  );
+}
+
+export default Root;
